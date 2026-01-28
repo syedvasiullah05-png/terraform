559af55e845645ffeca20e70a7aacc1ebacb066b
syedvasiullah05-png <syedvasiullah05@gmail.com>
2026-01-28 21:28:55 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,30 +1,9 @@
-variable "var_filename" {
-  description = "File path where the pet file will be created"
-  type        = string
-  default     = "pets.txt"
+variable "filename" {
+  type    = string
+  default = "animal.txt"
 }
 
-variable "var_content" {
-  description = "Content to be written into the pet file"
-  type        = string
-  default     = "My hero is black in colour"
+variable "content" {
+  type    = string
+  default = "hello this is vasi"
 }
-
-variable "random_pet_prefix" {
-  description = "Prefix for the random pet name"
-  type        = string
-  default     = "MR"
-}
-
-variable "random_pet_separator" {
-  description = "Separator used in the random pet name"
-  type        = string
-  default     = "-"
-}
-
-variable "random_pet_length" {
-  description = "Number of words in the random pet name"
-  type        = number
-  default     = 1
-}
-
