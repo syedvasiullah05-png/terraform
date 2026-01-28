4d7c6b0ea44c779bf3606ac6e6d3b379009e6b14
syedvasiullah05-png <syedvasiullah05@gmail.com>
2026-01-28 21:25:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,4 @@
-resource "random_pet" "my_pet" {
-  prefix    = "MR"
-  separator = "-"
-  length    = 1
-}
-
 resource "local_file" "my_pet" {
   filename = var.filename
-
-  # Create content using the random_pet id only once
-  content = "My pet name is: ${random_pet.my_pet.id}"
-
-  lifecycle {
-    prevent_destroy = true        # Never delete the file
-    ignore_changes  = [content]   # Never replace the file if content changes
-  }
+  content  = var.content
 }
-
