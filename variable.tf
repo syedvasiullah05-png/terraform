52f904faae21467c5c8c0ae7ca4985fdcce7263b
syedvasiullah05-png <syedvasiullah05@gmail.com>
2026-02-05 17:53:23 +0530

    Update variable definitions and add new variables

--- variable.tf
+++ variable.tf
@@ -1,9 +1,23 @@
 variable "filename" {
-  type    = string
-  default = "animal.txt"
+  description = "file name of project"
+  default     = "master.txt"
+  type        = string
 }
 
 variable "content" {
-  type    = string
-  default = "hello this is vasi"
+  description = "content of the project"
+  default     = "welcome to the Hyderabad"
+  type        = string
+}
+
+variable "prefix" {
+  description = "content of the prefix"
+  default     = "MR"
+  type        = string
+}
+
+variable "length" {
+  description = "length of the name"
+  default     = 1
+  type        = number
 }
